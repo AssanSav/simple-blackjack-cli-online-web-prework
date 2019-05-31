2a5f14bda27625081dba82826172de9963c94733
assane savadogo <assane.savadogo81@gmail.com>
2019-05-31 17:48:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ def hit?(current_total)
   prompt_user
   input = get_user_input
   if input == "h"
-    current_total += deal_card
+    current_card_total += deal_card
   elsif input == "s"
 else
   return current_total
